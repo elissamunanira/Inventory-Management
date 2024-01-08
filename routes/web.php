0135b24b018cdd5c2aa72c21f23d138a15f3c1db
elissamunanira <munanira64@gmail.com>
2024-01-08 19:37:11 +0200

    inventory

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', ['HomeController']);
+Route::get('/', [HomeController::class, 'home']);
 
 Route::group(['middleware' => ['auth']], function () {
   // Home
